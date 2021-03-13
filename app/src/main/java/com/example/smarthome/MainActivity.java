955b25affe0108ac74db4f95f4433a22879337e8
Panda <BRStar@123.com>
2021-03-13 17:35:40 +0800

    Add MqttService

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -16,10 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyMqttService.startService(this);
     }
 }
- */
+*/
+
 
 public class MainActivity extends ActivityMiniRecog {
 
+
 }
+
